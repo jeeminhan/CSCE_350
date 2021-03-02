bc0193c3f21568e959efc0e27dd200c140912a5f
jeeminhan <jeeminjameshan@gmail.com>
2021-03-02 15:07:34 -0600

    Adding lab03

---
+++ Lab03/main.c
@@ -0,0 +1,14 @@
+/* main.c simple program to test assembler program */
+
+#include <stdio.h>
+
+extern void lab03b();
+//extern void lab03c();
+
+int main(void)
+{
+    lab03b();
+
+    //lab03c();
+    return 0;
+}
